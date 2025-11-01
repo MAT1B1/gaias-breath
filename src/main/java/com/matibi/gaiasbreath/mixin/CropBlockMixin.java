bce64ff407e99bc35bb8940179f28df769596421
Matibi <mathias.bagal@gmail.com>
2025-11-01 12:31:34 +0100

    Mod menu config screen

--- src/main/java/com/matibi/gaiasbreath/mixin/CropBlockMixin.java
+++ src/main/java/com/matibi/gaiasbreath/mixin/CropBlockMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
